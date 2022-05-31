cdc8a866850621baaff6a19a814e8759018d72b5
AnahiTupiza <dtupizat@est.ups.edu.ec>
2022-05-31 10:34:29 -0500

    creada la clase persona

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 
+#include"persona.h"
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    cout << "Mi primer proyecto en Git y gitHub" << endl;
+    Persona p;
+    p.nombre ="anahi";
+    p.edad=19;
+    cout<< p.nombre<<endl;
+    cout<<p.edad<<endl;
     return 0;
 }
